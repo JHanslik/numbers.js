754c317b84ab3a98b685efa3518ae6097e6fe776
JHanslik <julien.hanslik@gmail.com>
2022-06-21 12:16:54 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+// 01 - Number
+// const integer = 102
+// const float = 13.9
+// console.log (integer, float)
+
+// 02 - Convert
+// const basic = 34
+// const stringified = basic.toString()
+// console.log (stringified)
+
+// 03 - Round
+// const num = 1.5
+// const rounded = Math.round(num)
+// console.log(rounded)
+
+// 04 - Arithmétique
+// const test = 12
+// const bis = 5
+// console.log(test + bis)
+// console.log(test - bis)
+// console.log(test / bis)
+// console.log(test * bis)
+// console.log(test % bis)
+
+
+// 05 - Reste division Euclidienne
+let number1 = 999
+
+for(let i = 1; i <= 5; i++) {
+    if(number1 % i == 0){
+       console.log(number1, "est divisible par", i);
+    }
+}
+
+let number2 = 25
+
+for(let i = 1; i <= 5; i++) {
+    if(number2 % i == 0){
+       console.log(number2, "est divisible par", i);
+    }
+}
+
+let number3 = 190280
+
+for(let i = 1; i <= 5; i++) {
+    if(number3 % i == 0){
+       console.log(number3, "est divisible par", i);
+    }
+}
+
+let number4 = 47
+
+for(let i = 1; i <= 5; i++) {
+    if(number4 % i == 0){
+       console.log(number4, "est divisible par", i);
+    }
+}
+
+let number5 = 691
+
+for(let i = 1; i <= 5; i++) {
+    if(number5 % i == 0){
+       console.log(number5, "est divisible par", i);
+    }
+}
